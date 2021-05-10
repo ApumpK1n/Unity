041ed50e803ef674cf5330a9fc121b6adc7df3e2
APumpKIN <30541379+ApumpK1n@users.noreply.github.com>
2021-05-10 21:37:15 +0800

    炸飞机

---
+++ BombPlane/Assets/Script/Game/Grid.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using UnityEngine.Tilemaps;
+using System.Collections;
+
+//A B C 
+//B
+//C
+
+public class Grid : MonoBehaviour
+{
+
+    public Tile planeTile;
+    public Tilemap tileMap;
+
+    private int col = 9;
+    private int row = 9;
+
+    private int[,] checkerboard;
+
+    private List<Plane> planes = new List<Plane>();
+
+    public void Start()
+    {
+        checkerboard = new int[row, col];
+        planes.Add(new Plane());
+
+        foreach (Plane plane in planes)
+        {
+            plane.SetData(checkerboard);
+        }
+        StartCoroutine(Draw());
+        //Draw();
+    }
+
+    public IEnumerator Draw()
+    {
+        Vector3Int currentCell = tileMap.WorldToCell(transform.position);
+
+        Vector3 tilePosition;
+        Vector3Int coordinate = new Vector3Int(0, 0, 0);
+        for (int i = 0; i < tileMap.size.x; i++)
+        {
+            for (int j = 0; j < tileMap.size.y; j++)
+            {
+                coordinate.x = i; coordinate.y = j;
+                tilePosition = tileMap.CellToWorld(coordinate);
+                TileBase tileBase = tileMap.GetTile(coordinate);
+                if (tileBase && tileBase is SwitchableTile)
+                {
+                    SwitchableTile tile = (SwitchableTile)tileBase;
+                    tile.sprite = tile.GetSprite(1);
+                }
+   
+                Debug.Log(string.Format("Position of tile [{0}, {1}] = ({2}, {3})", coordinate.x, coordinate.y, tilePosition.x, tilePosition.y));
+                yield return new WaitForSeconds(0.5f);
+                tileMap.RefreshTile(coordinate);
+            }
+        }
+    }
+}
---
+++ BombPlane/Assets/Script/Game/Plane.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum PlaneToward
+{
+    Top,
+    Bottom,
+    Right,
+    Left,
+}
+
+public class Plane : MonoBehaviour
+{
+    public int[,] Top = new int[4, 5]
+    {
+        { 0,0,2,0,0},
+        { 1,1,1,1,1},
+        { 0,0,1,0,0},
+        { 0,1,1,1,0},
+    };
+
+    public int[,] Bottom = new int[4, 5]
+    {
+        { 0,1,1,1,0},
+        { 0,0,1,0,0},
+        { 1,1,1,1,1},
+        { 0,0,2,0,0},
+    };
+
+    public int[,] Right = new int[5, 4]
+    {
+        { 0,0,1,0},
+        { 1,0,1,0},
+        { 1,1,1,2},
+        { 1,0,1,0},
+        { 0,0,1,0},
+    };
+
+    public int[,] Left = new int[5, 4]
+    {
+        { 0,1,0,0},
+        { 0,1,0,1},
+        { 2,1,1,1},
+        { 0,1,0,1},
+        { 0,1,0,0},
+    };
+
+    private Vector2Int headGridPos = new Vector2Int(0, 4);
+    private PlaneToward toward = PlaneToward.Top;
+
+    public bool SetData(int[,] checkerboard)
+    {
+        Vector2Int offset = new Vector2Int(0, 0);
+        int[,] Data = Top;
+        switch (toward)
+        {
+            case PlaneToward.Top:
+                offset = new Vector2Int(-2, 0);
+                Data = Top;
+                break;
+            case PlaneToward.Bottom:
+                offset = new Vector2Int(-2, -3);
+                Data = Bottom;
+                break;
+            case PlaneToward.Right:
+                offset = new Vector2Int(-3, -2);
+                Data = Right;
+                break;
+            case PlaneToward.Left:
+                offset = new Vector2Int(0, -2);
+                Data = Left;
+                break;
+        }
+
+        for (int i = 0; i < Data.GetLength(0); i++)
+        {
+            for (int j = 0; j < Data.GetLength(1); j++)
+            {
+                Vector2Int pos = headGridPos + offset + new Vector2Int(i, j);
+                if (pos.x >= 0 && pos.x < checkerboard.GetLength(0) && pos.y >= 0 && pos.y < checkerboard.GetLength(1))
+                {
+                    continue;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        for (int i = 0; i < Data.GetLength(0); i++)
+        {
+            for (int j = 0; j < Data.GetLength(1); j++)
+            {
+                Vector2Int pos = headGridPos + offset + new Vector2Int(i, j);
+                checkerboard[pos.x, pos.y] = Data[i, j];
+            }
+        }
+        return true;
+    }
+}
