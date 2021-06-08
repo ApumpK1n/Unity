9720e20bb13f52ff8a5c5bd4f5ecea575d69d431
APumpKIN <30541379+ApumpK1n@users.noreply.github.com>
2021-06-08 10:10:45 +0800

    add Bezier

---
+++ BezierCurve/BezierCurve.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class BezierCurve
+{
+
+    private Vector3 start;
+    private Vector3 end;
+    private Vector3 center;
+
+
+    public BezierCurve(Vector3 start, Vector3 center, Vector3 end)
+    {
+        this.start = start;
+        this.center = center;
+        this.end = end;
+    }
+
+
+    public Vector3 Sample(float t)
+    {
+        return Bezier3Point(start, center, end, t);
+    }
+
+    public Vector3 Bezier3Point(Vector3 start, Vector3 center, Vector3 end, float t)
+    {
+        return (1 - t) * (1 - t) * start + 2 * t * (1 - t) * center + t * t * end;
+    }
+}
---
+++ BezierCurve/BezierCurveMap.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public sealed class BezierCurveMap
+{
+    private readonly float[] _arcLengths;
+    private readonly float _ratio;
+    public float length { get; private set; }
+    public BezierCurve curve { get; private set; }
+    public bool isSet { get { return length != float.NaN; } }
+    public int resolution { get { return _arcLengths.Length; } }
+
+    public BezierCurveMap(int resolution)
+    {
+        _arcLengths = new float[resolution];
+        _ratio = 1f / resolution;
+        length = float.NaN;
+    }
+
+    public void Set(BezierCurve c)
+    {
+        curve = c;
+        Vector2 o = c.Sample(0);
+        float ox = o.x;
+        float oy = o.y;
+        float clen = 0;
+        int nSamples = _arcLengths.Length;
+        for (int i = 0; i < nSamples; i++)
+        {
+            float t = (i + 1) * _ratio;
+            Vector2 p = c.Sample(t);
+            float dx = ox - p.x;
+            float dy = oy - p.y;
+            clen += Mathf.Sqrt(dx * dx + dy * dy);
+            _arcLengths[i] = clen;
+            ox = p.x;
+            oy = p.y;
+        }
+        length = clen;
+    }
+
+    public Vector2 Sample(float u)
+    {
+        if (u <= 0) return curve.Sample(0);
+        if (u >= 1) return curve.Sample(1);
+
+        int index = 0;
+        int low = 0;
+        int high = resolution - 1;
+        float target = u * length;
+        float found = float.NaN;
+
+        // 找到距离近似值
+        while (low < high)
+        {
+            index = (low + high) / 2;
+            found = _arcLengths[index];
+            if (found < target)
+                low = index + 1;
+            else
+                high = index;
+        }
+
+        if (found > target)
+            index--;
+
+        if (index < 0) return curve.Sample(0);
+        if (index >= resolution - 1) return curve.Sample(1);
+
+        // 找出时间近似值并做一个修正
+        float min = _arcLengths[index];
+        float max = _arcLengths[index + 1];
+        float interp = (target - min) / (max - min);
+        return curve.Sample((index + interp + 0.5f) * _ratio);
+    }
+}
