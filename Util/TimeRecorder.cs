6a101d3c589776705b02f37fa0b2c8667c9bfae9
APumpKIN <30541379+ApumpK1n@users.noreply.github.com>
2021-06-18 16:38:02 +0800

    update

--- Util/TimeRecorder.cs
+++ Util/TimeRecorder.cs
@@ -10,6 +10,7 @@ public class TimeRecorder : SingletonBehaviour<TimeRecorder>
     private DateTime gameDateTimeNow;
 
     private float timeSinceLastSession;
+    private int preSavedDay;
     public DateTime GameDateTimeNow
     {
         get
@@ -18,14 +19,22 @@ public class TimeRecorder : SingletonBehaviour<TimeRecorder>
         }
     }
 
+    public static Action OnNewDay;
+    
+    private float DayTimer;
     private SaveData saveData;
 
     public class SaveData
     {
         public string savedGameDateTime;
         public double savedDeviceElapsedTime;
+        public int preSavedDay;
     }
-
+    public override void Awake()
+    {
+        base.Awake();
+        DontDestroyOnLoad(this);
+    }
     public void LoadSaveData()
     {
         if (ES3.KeyExists(saveKey))
@@ -43,6 +52,7 @@ public class TimeRecorder : SingletonBehaviour<TimeRecorder>
 
         saveData.savedGameDateTime = GameDateTimeNow.ToString(CultureInfo.InvariantCulture);
         saveData.savedDeviceElapsedTime = GetDeviceElapsedTime();
+        saveData.preSavedDay = preSavedDay;
 
         ES3.Save(saveKey, saveData);
 
@@ -50,6 +60,28 @@ public class TimeRecorder : SingletonBehaviour<TimeRecorder>
         Debug.Log("TimeRecorder: savedGameDateTime:" + saveData.savedGameDateTime);
         Debug.Log("TimeRecorder: savedDeviceElapsedTime:" + saveData.savedDeviceElapsedTime);
 #endif
+    }
+
+    private void Update()
+    {
+        DayTimer += Time.deltaTime;
+        if (DayTimer > 10f)
+        {
+            DayTimer = 0;
+            if (preSavedDay != GameDateTimeNow.DayOfYear)
+            {
+                UpdateDay();
+            }
+        }
+    }
+
+    private void UpdateDay()
+    {
+        if (preSavedDay != GameDateTimeNow.DayOfYear)
+        {
+            preSavedDay = GameDateTimeNow.DayOfYear;
+            OnNewDay?.Invoke();
+        }
     }
 
     private void OnEnable()
@@ -104,7 +136,12 @@ public class TimeRecorder : SingletonBehaviour<TimeRecorder>
         }
         else gameDateTimeNow = DateTime.Now;
 
+        if (saveData == null) preSavedDay = GameDateTimeNow.DayOfYear;
+        else preSavedDay = saveData.preSavedDay;
+
         Debug.Log("Recorder: Time since last session: " + timeSinceLastSession);
+
+        UpdateDay();
 
         OnSave();
     }
@@ -165,10 +202,26 @@ public class TimeRecorder : SingletonBehaviour<TimeRecorder>
     {
 #if UNITY_IOS
         long elapsedTime = Uptime.RequestiOSDeviceUptime();
+        Debug.Log("elapsedTime:" + elapsedTime);
         return elapsedTime;
 #else
         return -1;
 #endif
     }
     #endregion
+
+#if UNITY_EDITOR
+    public void AddExtraSeconds(double seconds)
+    {
+        gameDateTimeNow = gameDateTimeNow.AddSeconds(seconds);
+        Debug.Log("[TIME CONTROLLER] Debug set new time: " + gameDateTimeNow.ToString());
+    }
+
+    public void SetTimeToEndOfDay(int secondsBefore)
+    {
+        DayTimer = 0;
+        gameDateTimeNow = gameDateTimeNow.Date.AddDays(1).AddSeconds(-secondsBefore);
+        Debug.Log("[TIME CONTROLLER] Debug set time to end of day: " + gameDateTimeNow.ToString());
+    }
+#endif
 }
---
+++ Util/TimeRecorderEditor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(TimeRecorder))]
+public class TimeRecorderEditor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if (GUILayout.Button("Set end of day date"))
+        {
+            TimeRecorder timeController = (TimeRecorder)target;
+            timeController.SetTimeToEndOfDay(1);
+        }
+
+        if (GUILayout.Button("Add 60 seconds"))
+        {
+            TimeRecorder timeController = (TimeRecorder)target;
+            timeController.AddExtraSeconds(60);
+        }
+
+        if (GUILayout.Button("Add 3600 seconds"))
+        {
+            TimeRecorder timeController = (TimeRecorder)target;
+            timeController.AddExtraSeconds(3600);
+        }
+    }
+}
